6fa45ca86f35c771e0fd57822bc73043a86ecb70
Alexsander Antunes <alexsandercaac@gmail.com>
2023-07-28 23:36:54 -0300

    Added all initial datasets to DVC pipeline
    * MNIST
    * Caltech101
    * CIFAR10
    * OxfordIIITPet

--- src/download_data.py
+++ src/download_data.py
@@ -3,8 +3,25 @@
 """
 import torchvision
 
+
+ROOT = 'data/raw'
 # Data is saved in the data/raw folder
 mnist_dataset = torchvision.datasets.MNIST(
-    root='data/raw',
+    root=ROOT,
+    download=True
+)
+
+cifar10_dataset = torchvision.datasets.CIFAR10(
+    root=ROOT,
+    download=True
+)
+
+caltech101_dataset = torchvision.datasets.Caltech101(
+    root=ROOT,
+    download=True
+)
+
+oxfordiit_dataset = torchvision.datasets.OxfordIIITPet(
+    root=ROOT,
     download=True
 )
